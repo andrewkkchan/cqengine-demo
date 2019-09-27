9c358efeb653a0d8e2e098850f9e01f36ba3f136
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-09-27 15:43:22 +0800

    add car factory

--- src/main/java/com/industrieit/cqengine/demo/DemoApplication.java
+++ src/main/java/com/industrieit/cqengine/demo/DemoApplication.java
@@ -5,9 +5,14 @@ import com.googlecode.cqengine.IndexedCollection;
 import com.googlecode.cqengine.index.hash.HashIndex;
 import com.googlecode.cqengine.query.Query;
 import com.googlecode.cqengine.resultset.ResultSet;
+import com.industrieit.cqengine.demo.factory.CarFactory;
+import com.industrieit.cqengine.demo.model.Car;
 import com.industrieit.cqengine.demo.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static com.googlecode.cqengine.query.QueryFactory.endsWith;
 import static com.googlecode.cqengine.query.QueryFactory.equal;
@@ -16,6 +21,10 @@ import static com.sun.tools.doclint.Entity.or;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    static final int COLLECTION_SIZE = 100000;
+    static final int WARMUP_REPETITIONS = 10000;
+    static final int MEASUREMENT_REPETITIONS = 10000;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -26,6 +35,8 @@ public class DemoApplication {
         for (User user : accountant){
             System.out.println(user);
         }
+        Collection<Car> collection = CarFactory.createCollectionOfCars(COLLECTION_SIZE);
+
     }
 
 }
